4fbd912bdfeea7a7457c42fe0dd4fa4d398aac70
AngelBzDev <angelbaez.dev@gmail.com>
2022-05-05 00:14:05 -0500

    Mejoras

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 document.querySelector("#btn__menu").addEventListener("click", () => {
   const navbarMenu = document.querySelector(".navbar__options");
   if (navbarMenu.classList.contains("hidden")) {
-    navbarMenu.classList.remove("hidden");
-    navbarMenu.classList.add("flex");
+    navbarMenu.classList.replace("hidden", "flex");
   } else {
-    navbarMenu.classList.remove("flex");
-    navbarMenu.classList.add("hidden");
+    navbarMenu.classList.replace("flex", "hidden");
   }
+  navbarMenu.classList.toggle("animate__fadeInLeft");
 });
