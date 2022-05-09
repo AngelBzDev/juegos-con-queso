40328a5ed0f641db8fdf9d18009196e6c8e6c95f
AngelBzDev <angelbaez.dev@gmail.com>
2022-05-08 19:15:55 -0500

    Tarjetas de juegos populares

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+//Boton para abrir la navegacion
 document.querySelector("#btn__menu").addEventListener("click", () => {
   const navbarMenu = document.querySelector(".navbar__options");
   if (navbarMenu.classList.contains("hidden")) {
@@ -5,13 +6,13 @@ document.querySelector("#btn__menu").addEventListener("click", () => {
   } else {
     navbarMenu.classList.replace("flex", "hidden");
   }
-  /* navbarMenu.classList.toggle("animate__fadeInLeft"); */
 });
 
 //Swiper
 const swiperContainer = document.querySelector(".swiper-wrapper");
-const slides = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-const url = "https://www.lavanguardia.com/files/og_thumbnail/files/fp/uploads/2022/02/08/620280c33764b.r_d.3246-2700-0.jpeg"
+const slides = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const url =
+  "https://www.lavanguardia.com/files/og_thumbnail/files/fp/uploads/2022/02/08/620280c33764b.r_d.3246-2700-0.jpeg";
 
 slides.forEach((slide) => {
   const slideDiv = document.createElement("div");
@@ -58,8 +59,61 @@ const swiper = new Swiper(".mySwiper", {
     1440: {
       slidesPerView: 8,
       spaceBetween: 20,
-    }
+    },
   },
 });
 
+const getGames = async (size) => {
+  try {
+    const respuesta = await fetch(
+      `https://api.rawg.io/api/games?key=48add53f7d224aae88ebe6826f55af90&page_size=${size}`,
+      {
+        method: "GET",
+      }
+    );
+    const resultado = await respuesta.json();
+    return resultado.results;
+  } catch (err) {
+    return err;
+  }
+};
 
+console.log(await getGames());
+
+//Tarjetas de juegos populares
+const popularGamesContainer = document.querySelector(".popular__games");
+let juegos;
+if (popularGamesContainer.classList.contains("four")) {
+  juegos = await getGames(4);
+} else {
+  juegos = await getGames(8);
+}
+
+juegos.forEach((juego, i) => {
+  const card = document.createElement("div");
+  card.innerHTML = `<div class="popular__game">
+  <div class="popular__game-info">
+    <img
+      class="popular__game-img"
+      src="${juego.background_image}"
+      alt="${juego.slug}"
+    />
+    <div class="popular__game-desc">
+      <h4>${juego.name}</h4>
+      <p>${juego.genres[0].name}</p>
+      <p>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+      </p>
+    </div>
+  </div>
+  <p>MXN$1099</p>
+</div>`;
+  popularGamesContainer.appendChild(card);
+});
+
+//Api
+/* 48add53f7d224aae88ebe6826f55af90 */
